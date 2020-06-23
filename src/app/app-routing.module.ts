a68de6547507c08842294fe663bbcfd8d185e5b0
Chanel Letinić <chanelletinic@gmail.com>
2020-06-23 17:40:37 +0200

    need to fix grid

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { Routes, RouterModule } from '@angular/router';
 
 // Screens
 import { HomeComponent } from './screens/home/home.component';
+import { LoginComponent } from './screens/login/login.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full'},
   { path: 'home', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
 ];
 
 @NgModule({
