2e7aedbc1ef8f496d0bc9d2e9f8173c0d8b3c30d
Marius Ludick <ludickmarius@gmail.com>
2020-07-10 00:17:56 +0200

    Re-Added autologin with refresh working for lobbies

--- src/app/services/auth/auth.service.ts
+++ src/app/services/auth/auth.service.ts
@@ -52,6 +52,33 @@ export class AuthService {
       );
   }
 
+  autoLogin() {
+    const userData: {
+      email: string;
+      id: string;
+      _token: string;
+      _tokenExpirationDate: string;
+    } = JSON.parse(localStorage.getItem('userData'));
+    if (!userData) {
+      return;
+    }
+
+    const loadedUser = new User(
+      userData.email,
+      userData.id,
+      userData._token,
+      new Date(userData._tokenExpirationDate)
+    );
+
+    if (loadedUser.token) {
+      this.user.next(loadedUser);
+      const expirationDuration =
+        new Date(userData._tokenExpirationDate).getTime() -
+        new Date().getTime();
+      this.autoLogout(expirationDuration);
+    }
+  }
+
   login(email: string, password: string) {
     return this.http
       .post<AuthResponseData>(
