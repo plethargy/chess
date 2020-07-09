e304563981596fa9afab252dec1880e1d43bb918
jonathanc <jonathanc@bbd.co.za>
2020-07-09 20:51:30 +0200

    Added BE method to emit event for gameOver

--- backend/server.js
+++ backend/server.js
@@ -130,6 +130,8 @@ function move(sessionID, fromPosition, toPosition) {
 
   let move = game.move({ from: fromPosition, to: toPosition });
 
+  checkGameOver(sessionID);
+
   console.log(move);
 
   return move;
@@ -164,6 +166,19 @@ function move(sessionID, fromPosition, toPosition) {
   //   //res.json(move);
   // }
 
+}
+
+function checkGameOver(sessionID) {
+  let session = Sessions[sessionID];
+  let game = session['State'];
+  if (game.checkGameOver()) {
+    let winner;
+    if (game.turn === "w")
+      winner = game.White;
+    else
+      winner = game.Black;
+    socket.emit("gameOver", winner);
+  }
 }
 
 function getMoves(sessionID, position) {
